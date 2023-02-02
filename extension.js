8ccd1a7586fdd287e5a60b08c6d3bbd3b7fb0e21
Lolemo14 <correoedgar14@gmail.com>
2023-02-02 19:16:14 +0100

    Update extension.js
    Added custom display size support.
    Added "Set width and height" block

--- extension.js
+++ extension.js
@@ -1,74 +1,99 @@
+
+
 class multiTouch {
-    getInfo() {
-        return {
-            id: 'multiTouch',
-            color1: '#2457b5',
-            color2: '#2457b5',
-            color3: '#5285e3',
-            name: 'Multi-Touch',
-            blocks: [{
-                opcode: 'coord',
-                text: 'Coordenada [thing] del cursor [target] ',
-                blockType: Scratch.BlockType.REPORTER,
-                arguments: {
-                    thing: {
-                        type: Scratch.ArgumentType.STRING,
-                        defaultValue: "X",
-                        menu: 'thing'
-                    },
-                    target: {
-                        type: Scratch.ArgumentType.NUMBER,
-                        defaultValue: "0"
-                    }
-                }
-            }],
-            menus: {
-                thing: {
-                    acceptReporters: true,
-                    items: [{
-                            text: 'X',
-                            value: "X"
-                        },
-                        {
-                            text: 'Y',
-                            value: "Y"
-                        }
-
-                    ]
-                }
+  getInfo() {
+    return {
+      id: 'multiTouch',
+      color1: '#2457b5',
+      color2: '#2457b5',
+      color3: '#5285e3',
+      name: 'Multi-Touch',
+      blocks: [
+        {
+          opcode: 'coord',
+          text: 'Coordenada [thing] del cursor [target] ',
+          blockType: Scratch.BlockType.REPORTER,
+          arguments: {
+            thing: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "X",
+              menu: 'thing'
+            },
+            target: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: "0"
             }
-        };
-    }
+          }
+        },
 
-    coord({
-        thing,
-        target
-    }) {
+        {
+          opcode: 'setDimensions',
+          text: 'Set width to [width] and height to [height]',
+          blockType: Scratch.BlockType.COMMAND,
+          arguments: {
+            width: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '480'
+            },
+             height: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '360'
+            }
+          }
+        }
+        
+      ],
+      menus: {
+          thing: {
+            acceptReporters: true,
+            items: [
+              {
+                text: 'X',
+                value: "X"
+              },
+              {
+                text: 'Y',
+                value: "Y"
+              }
+              
+            ]
+          }
+      }
+  };
+}
+  
+  coord({ thing, target }) {
+    
+    let XY = 0;
+    let x = 0;
+    let y = 0;
+    let num = target;
 
-        let XY = 0;
-        let num = target;
+    
         if (thing === "X") {
-            return new Promise((resolve, reject) => {
-                document.addEventListener("touchmove", function(event) {
-                    XY = event.touches[num].clientX;
-                    XY = (XY - '240');
-                    resolve(XY);
-                });
-            });
-        }
-
-        if (thing === "Y") {
-            return new Promise((resolve, reject) => {
-                document.addEventListener("touchmove", function(event) {
-                    XY = event.touches[num].clientY;
-                    XY = (XY - '400') * "-1";
-                    resolve(XY);
-                });
-            });
-        }
-
+        return new Promise((resolve, reject) => {
+        document.addEventListener("touchmove", function(event) {
+        XY = event.touches[num].clientX;
+        XY = (XY - w/2);
+        resolve(XY);
+        });
+        });
+      };
+      if (thing === "Y") {
+        return new Promise((resolve, reject) => {
+        document.addEventListener("touchmove", function(event) {
+        XY = event.touches[num].clientY;
+        XY = (XY - h/2) * "-1";
+        resolve(XY);
+        });
+        });
+      };
+    
+  }
+  setDimensions ({ width, height }) {
+      window.w = width;
+      window.h = height;
     }
-
 }
 
 Scratch.extensions.register(new multiTouch());
